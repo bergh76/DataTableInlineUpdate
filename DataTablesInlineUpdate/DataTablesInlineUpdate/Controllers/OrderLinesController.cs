03c50b1c68027c05f05cb6622f3dd73b3cc85a1b
Andreas <andreas__bergh@hotmail.com>
2017-01-06 12:56:19 +0100

    tjo

--- DataTablesInlineUpdate/DataTablesInlineUpdate/Controllers/OrderLinesController.cs
+++ DataTablesInlineUpdate/DataTablesInlineUpdate/Controllers/OrderLinesController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
 using DataTablesInlineUpdate.Models;
 
 namespace DataTablesInlineUpdate.Controllers
@@ -80,18 +81,78 @@ namespace DataTablesInlineUpdate.Controllers
         // POST: OrderLines/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+        public ActionResult OrderFilterOrderDate(string dateQ)
+        {
+          
+
+            //if (Request.IsAjaxRequest())
+            //{
+            //    var q = dateQ;
+            //    if (String.IsNullOrEmpty(q))
+            //    {
+            //        var emptyorders = new List<Order>();
+            //        model.Orders = emptyorders;
+            //        ViewBag.Filter = "No date added";
+            //        return PartialView("_OrderList", model);
+            //    }
+            //    try
+            //    {
+            //        var fromDate = q.Substring(0, 10);
+            //        var toDate = q.Substring(13, 10);
+
+            //        var sDate = Convert.ToDateTime(fromDate);
+            //        var eDate = Convert.ToDateTime(toDate);
+
+            //        var orders =
+            //            db.Orders.Where(s => s.Status != OrderStatus.Makulated)
+            //                .Where(d => d.CreatedDate >= sDate && d.CreatedDate <= eDate)
+            //                .Include(ii => ii.Customer)
+            //                .Include(o => o.OrderLines)
+            //                .ToList();
+            //        model.Orders = orders;
+            //        ViewBag.Filter = "Filtered on created date";
+            //        return PartialView("_OrderList", model);
+
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        return RedirectToAction("Index", "Manage");
+
+            //    }
+
+
+
+            //}
+
+            return null;
+
+        }
+        
+        public JsonResult Tester(string artNum, string orderlineid, int numberofitems, string deliverydate)
+        {
+            if (Request.IsAjaxRequest())
+            {
+
+                TempData["IsFixd"] = "true";
+
+                return Json("", JsonRequestBehavior.AllowGet);
+            }
+            return Json("", JsonRequestBehavior.AllowGet);
+        }
+
+
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(OrderLine orderLine, FormCollection form,  int id)
+        public ActionResult Edit(string artNum, string orderlineid, int numberofitems, string deliverydate)
         {
             if (ModelState.IsValid)
             {
-                string aId = form[1].ToString();
-                var artId = db.Articles.Where(x => x.ArticleNumber == aId).Select(x => x.ArticleId).SingleOrDefault();
-                orderLine = db.OrderLines.Find(id);
-                orderLine.ArticleId = Convert.ToInt32(artId);
-                db.Entry(orderLine).State = EntityState.Modified;
-                db.SaveChanges();
+         
+                //var artId = db.Articles.Where(x => x.ArticleNumber == aId).Select(x => x.ArticleId).SingleOrDefault();
+                //var orderLine = db.OrderLines.Find(id);
+                //orderLine.ArticleId = Convert.ToInt32(artId);
+                //db.Entry(orderLine).State = EntityState.Modified;
+                //db.SaveChanges();
                 return RedirectToAction("Index");
             }
             //ViewBag.ArticleId = new SelectList(db.Articles, "ArticleId", "ArticleNumber", orderLine.ArticleId);
@@ -112,7 +173,7 @@ namespace DataTablesInlineUpdate.Controllers
             }
             return View(orderLine);
         }
-
+        
         // POST: OrderLines/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -133,28 +194,34 @@ namespace DataTablesInlineUpdate.Controllers
             base.Dispose(disposing);
         }
 
-        [HttpPost]
         public JsonResult GetArticleNrValue(string artnrVal)
         {
-            var s = db.Articles.Where(w => w.ArticleNumber
+            if (Request.IsAjaxRequest())
+            {
+                var s = db.Articles.Where(w => w.ArticleNumber
                 .ToLower()
                 .Contains(artnrVal.ToLower()
                 )
             )
             .ToList();
-            return Json(s, JsonRequestBehavior.AllowGet);
+                return Json(s, JsonRequestBehavior.AllowGet);
+            }
+            return Json(false);
         }
 
-        [HttpPost]
         public JsonResult GetArticleName(string artName)
         {
-            var s = db.Articles.Where(w => w.ArticleName
+            if (Request.IsAjaxRequest())
+            {
+                var s = db.Articles.Where(w => w.ArticleName
                 .ToLower()
                 .Contains(artName.ToLower()
                 )
             )
             .ToList();
-            return Json(s, JsonRequestBehavior.AllowGet);
+                return Json(s, JsonRequestBehavior.AllowGet);
+            }
+            return Json(false);
         }
     }
 }
--- DataTablesInlineUpdate/DataTablesInlineUpdate/Global.asax.cs
+++ DataTablesInlineUpdate/DataTablesInlineUpdate/Global.asax.cs
@@ -18,6 +18,7 @@ namespace DataTablesInlineUpdate
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            HtmlHelper.UnobtrusiveJavaScriptEnabled = true;
         }
     }
 }
